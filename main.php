d7f4e6a336e133eb26cec37fe16dc09ad4bf8602
Denis Balebanov <balebanov@users.noreply.github.com>
2016-09-15 00:07:21 +0400

    Main page

--- main.php
+++ main.php
@@ -2,28 +2,31 @@
 <html>
 <head>
 	<script src="//vk.com/js/api/openapi.js" type="text/javascript"></script>
-	<script type="text/javascript">
-  		VK.init({
-    	apiId: Your App ID
-  		});
-	</script>
+	<script src="vk_init.js" type="text/javascript"></script>
+	
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 	<title>VK</title>
+	<link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <body>
-	<script type="text/javascript" charset="utf-8">
-	VK.Auth.login(function(response) {
-		if (response.session) {
-			document.write("OK");
-			if (response.settings) {
-				document.write("response");
-			}
-		} else {
-			document.write("NotOK");
-			}
-		});
-
-	VK.Api.call('wall.post', {owner_id:'128605250', friends_only: 1, message: "I'm trying to work with OpenAPI by VK. Cool thing. It doesn't support Russian language yet, but soon I'll fix it. Anyway, I'm writing it only using my own web-server :)"}, function(r){}
-	);
-	</script>
+		<center><h1>Клиент ВК на localhost</h1></center>
+		<table border="1" class="table">
+			<tr>
+				<td>
+					<div class="menu">
+						<a href="get_post.php?link=123">Информация</a><br>
+						<a href="get_post.php">Диалоги</a>
+					</div>
+				</td>
+				<td>
+					<div class="main_info">Main info</div>
+				</td>
+			</tr>
+		</table>
+		<div class="footer">
+			<hr>
+			<h5>By Denis Balebanov @denisonl</h5><br>
+		</div> 
 </body>
 </html>
